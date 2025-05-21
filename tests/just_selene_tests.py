c612db7c34b873ac1e346cca59d9834ff8f3d4ab
diana.ermilova <diana.ermilova.96@gmail.com>
2025-05-21 22:08:59 +0300

    Коммит тестов с чистым selene

---
+++ tests/just_selene_tests.py
@@ -0,0 +1,11 @@
+from selene import browser, by, be
+from selene.support.shared.jquery_style import s
+
+def test_open_browser():
+    browser.open("https://github.com")
+    browser.driver.fullscreen_window()
+    browser.element('.search-input').click()
+    browser.element('#query-builder-test').send_keys("eroshenkoam/allure-integration-example").press_enter()
+    browser.element(by.link_text("eroshenkoam/allure-integration-example")).press_enter()
+    browser.element('#issues-tab').click()
+    s(by.text("No results")).should(be.visible)
